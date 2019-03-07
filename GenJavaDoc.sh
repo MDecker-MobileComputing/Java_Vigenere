90db5a089df9253d7e65a3fa51e3546a84f9f0b7
MDecker-MobileComputing <mide_42b@yahoo.de>
2019-03-07 16:18:04 +0100

    chmod u+x *.sh; CRLF->LF fuer eine der Bash-Dateien.

--- GenJavaDoc.sh
+++ GenJavaDoc.sh
@@ -6,5 +6,5 @@
 mkdir -p javadoc
 # -p: Keinen Fehler ausgeben, wenn das Verzeichnis schon existiert
 
-javadoc  -private -doctitle "Vigenere-Chiffre" -d javadoc -sourcepath src -charset "UTF-8" -encoding "UTF-8" -docencoding "UTF-8" -subpackages de.mide.vigenere -html5
+javadoc  -private -doctitle "Vigenere-Chiffre" -d javadoc -sourcepath src -charset "UTF-8" -encoding "UTF-8" -docencoding "UTF-8" -subpackages de.mide.vigenere #-html5
         
